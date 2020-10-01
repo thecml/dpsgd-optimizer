b4b44f8904dfdd967238c6fa37168070c63218a2
thecml <chr1000@gmail.com>
2020-10-01 16:31:40 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import os
 from setuptools import find_packages, setu
 
 setup(
-    name='lego-keras-gan',
+    name='dpsgd-optimizer',
 	packages=find_packages(),
     version='0.0.0',
-    description='A GAN to create images of LEGO figures.',
-    url='https://github.com/thecml/lego-keras-gan.git',
-    author='Christian Lillelund',
+    description='Differential Private SGD implementation.',
+    url='https://github.com/thecml/dpsgd-optimizer',
+    author='Christian Lillelund, Michael Quach',
     license='MIT'
 )
