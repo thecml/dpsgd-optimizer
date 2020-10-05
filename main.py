f81636d8c29a1c781ee8a383851f5661d42020ea
thecml <chr1000@gmail.com>
2020-10-05 19:05:21 +0200

    works for eps and delta now

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ from accountant import GaussianMomentsAccountant
 EpsDelta = collections.namedtuple("EpsDelta", ["spent_eps", "spent_delta"])
 IMAGE_SIZE = 28
 TARGET_EPS = [0.125, 0.25, 0.5, 1, 2, 4, 8]
+TARGET_DELTA = [1e-2]
+BATCH_SIZE = 64
+C = 4.0
 
 def make_model():
     model = keras.Sequential([
@@ -21,12 +24,14 @@ def make_model():
     ])
     return model
 
-class DummySanitizer(object):
+class GaussianSanitizer(object):
   """An sanitizer that does no sanitizing."""
 
   def sanitize(self, gradients, eps_delta, sigma):
+      gradients = [tf.clip_by_norm(g, clip_norm=C) for g in gradients]
+      gradients += np.random.normal(0, (sigma ** 2)*(C ** 2), len(gradients))
       return gradients
-
+      
 def make_model():
     model = keras.Sequential([
         keras.layers.Flatten(input_shape=(28, 28)),
@@ -35,7 +40,6 @@ def make_model():
     ])
     return model
 
-#@keras_export("keras.optimizers.DPSGD")
 class DPSGD_Optimizer(tf.optimizers.SGD):
     """Differentially private gradient descent optimizer."""
     def __init__(self, learning_rate, accountant, sanitizer, use_locking=False, name="DPSGD_Optimizer"):
@@ -43,11 +47,10 @@ class DPSGD_Optimizer(tf.optimizers.SGD):
         self._accountant = accountant
         self._sanitizer = sanitizer
 
-    def minimize(self, loss, weights, batch_size, eps_delta, sigma, tape):
+    def minimize(self, batch_size, gradients, weights, eps_delta, sigma):
         priv_accum_op = self._accountant.accumulate_privacy_spending(eps_delta, sigma, batch_size)
         with tf.control_dependencies(priv_accum_op):
-            gradients = tape.gradient(loss, weights)
-            sanitized_grad = self._sanitizer.sanitize(gradients, eps_delta, sigma)
+            gradients[1] = self._sanitizer.sanitize(gradients[1], eps_delta, sigma)
             return self.apply_gradients(zip(gradients, weights))
 
 def main():
@@ -60,7 +63,6 @@ def main():
     valid_acc_metric = keras.metrics.SparseCategoricalAccuracy()
     
     # Prepare the training dataset.
-    batch_size = 64
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
     x_train = np.reshape(x_train, (-1, IMAGE_SIZE*IMAGE_SIZE))
     x_train = x_train.astype("float32") / 255.0
@@ -73,19 +75,21 @@ def main():
     x_train = x_train[:10000]
     y_train = y_train[:10000]
     valid_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-    valid_dataset = valid_dataset.batch(batch_size)
+    valid_dataset = valid_dataset.batch(BATCH_SIZE)
     train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
+    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(BATCH_SIZE)
     
     # Run training loop
-    epochs = 5
+    epochs = 10
     sigma = 4
     max_eps = 2.0
     max_delta = 1e-05
+    #max_eps = 5.0
+    #max_delta = 1e-03
     eps_delta = EpsDelta(np.inf, 1.0)
     
     accountant = GaussianMomentsAccountant(len(x_train))
-    sanitizer = DummySanitizer()
+    sanitizer = GaussianSanitizer()
     dp_opt = DPSGD_Optimizer(0.01, accountant, sanitizer)
     
     for epoch in range(epochs):
@@ -95,17 +99,18 @@ def main():
             with tf.GradientTape() as tape:
                 logits = model(x_batch_train, training=True)
                 loss_value = loss_fn(y_batch_train, logits)
-                spent_eps_deltas = EpsDelta(0, 0)
-                max_target_eps = max(TARGET_EPS)
-                while spent_eps_deltas.spent_eps <= max_eps and spent_eps_deltas.spent_delta <= max_delta:
-                    dp_opt.minimize(loss_value, model.weights, batch_size, eps_delta, sigma, tape)
-                    spent_eps_deltas = accountant.get_privacy_spent(target_eps=TARGET_EPS)[0]
-                print(f"Spent privacy: eps {spent_eps_deltas.spent_eps} delta {spent_eps_deltas.spent_delta}")
-                train_acc_metric.update_state(y_batch_train, logits)
-                if step % 200 == 0:
-                    print(f"Training loss at step: {step}: {float(loss_value)}")
-                    print(f"So far trained on {(step+1) * 64} samples")
-                    
+            gradients = tape.gradient(loss_value, model.weights)
+            spent_eps_deltas = EpsDelta(0, 0)
+            max_target_eps = max(TARGET_EPS)
+            while spent_eps_deltas.spent_eps <= max_eps and spent_eps_deltas.spent_delta <= max_delta:
+                dp_opt.minimize(BATCH_SIZE, gradients, model.weights, eps_delta, sigma)
+                spent_eps_deltas = accountant.get_privacy_spent(target_deltas=TARGET_DELTA)[0]
+            train_acc_metric.update_state(y_batch_train, logits)
+            if step % 200 == 0:
+                print(f"Training loss at step: {step}: {float(loss_value)}")
+                print(f"So far trained on {(step+1) * 64} samples")
+        
+        print(f"Spent privacy: eps {spent_eps_deltas.spent_eps}, delta {spent_eps_deltas.spent_delta}")    
         train_acc = train_acc_metric.result()
         print(f"Training acc over epoch: {float(train_acc)}")
         train_acc_metric.reset_states()
