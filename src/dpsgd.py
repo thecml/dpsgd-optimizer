4f81286030785f83c0bca6387b0481186a94a55e
thecml <chr1000@gmail.com>
2020-10-17 16:44:51 +0200

    did some testing with values

--- src/dpsgd.py
+++ src/dpsgd.py
@@ -10,7 +10,7 @@ from sanitizer import *
 EpsDelta = collections.namedtuple("EpsDelta", ["spent_eps", "spent_delta"])
 IMAGE_SIZE = 28
 N_CHANNELS = 1
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 LEARNING_RATE = 0.05
 L2NORM_BOUND = 4.0
 
@@ -64,15 +64,14 @@ def main():
 
     # Set constants for this loop
     epochs = 10
-    sigma = 5.0
-    eps = 1.0
-    delta = 1e-5
-    max_eps = 8.0
+    sigma = 2.0
+    eps = 4.0
+    delta = 1e-7
+    max_eps = 16.0
     max_delta = 1e-5
-    target_eps = [8]
+    target_eps = [1]
     target_delta = [1e-5]
     total_samples = len(x_train)
-    spent_eps_deltas = EpsDelta(0, 0)
     
     # Create objects
     accountant = AmortizedAccountant(total_samples)
@@ -102,7 +101,7 @@ def main():
                 sanitized_grads = []
                 eps_delta = EpsDelta(eps, delta)
                 for px_grad in gradients:
-                    sanitized_grad = sanitizer.sanitize(px_grad, eps_delta, num_examples=1)
+                    sanitized_grad = sanitizer.sanitize(px_grad, eps_delta, sigma, num_examples=1)
                     sanitized_grads.append(sanitized_grad)
                 spent_eps_deltas = accountant.get_privacy_spent(target_eps=target_eps)[0]
                 optimizer.apply_gradients(zip(sanitized_grads, train_vars))
@@ -114,17 +113,13 @@ def main():
             spent_eps = spent_eps_deltas.spent_eps
             spent_delta = spent_eps_deltas.spent_delta
             train_acc = train_acc_metric.result()
-            train_scores.append(train_acc)
             train_acc_metric.reset_states()
-            batch_valid_loss = list()
+            
             for x_batch_valid, y_batch_valid in valid_dataset:
                 valid_logits = model(x_batch_valid, training=False)
                 valid_acc_metric.update_state(y_batch_valid, valid_logits)
-                batch_valid_loss.append(loss_fn(y_batch_valid, valid_logits))
-            valid_loss.append(np.mean(batch_valid_loss))
             valid_acc = valid_acc_metric.result()
             valid_acc_metric.reset_states()
-            valid_scores.append(valid_acc)
             time_taken = time.time() - start_time
             print(f"Epoch {epoch + 1}, trained on {num_samples} samples, batch loss: {batch_loss:{2}.{4}}, " +
                    f"eps: {spent_eps:{2}.{4}}, delta: {spent_delta:{2}.{4}}, training acc: {train_acc:{2}.{4}}, " +
